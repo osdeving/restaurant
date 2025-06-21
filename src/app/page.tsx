7c3b65831986b5e4b7b5ab43397a826f7088f067
Willams Sousa <osdeving@gmail.com>
2025-06-21 03:54:01 -0300

    feat: main content part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import Image from "next/image";
-import MenuPage from "./menu/page";
+
+import Slider from "../components/Slider";
+import Featured from "../components/Featured";
+import Offer from "../components/Offer";
 
 export default function Home() {
   return (
   <main >
-    Hello world!
-    <Image
-      src="/cart.png"
-      alt="Cart Icon"
-      width={50}
-      height={50}
-    />
-    <MenuPage />
+    <Slider />
+    <Featured />
+    <Offer />
   </main>
   );
 }
