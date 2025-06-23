83d141e9cb9f668e16f699f4ce2cc7e0a7ffbbf9
Willams Sousa <osdeving@gmail.com>
2025-06-23 01:00:02 -0300

    feat: criar cart page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,10 +1,32 @@
 
 import React from 'react'
+import { singleProduct } from '../../../data'
+import Price from '../../../components/Price'
+import Image from 'next/image'
 
 const SingleProductPage = () => {
   return (
-    <div>SingleProductPage</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center'>
+      {/* IMAGE CONTAINER */}
+      <div className='flex-1 relative w-full h-1/2 md:h-[70%]'>
+        {singleProduct.img && (
+          <Image
+            src={singleProduct.img}
+            alt={singleProduct.title}
+            fill
+            className='object-contain'
+          />
+        )}
+      </div>
+
+      {/* TEXT CONTAINER */}
+      <div className='h-1/2 flex-1 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8'>
+        <h1 className='text-3xl font-bold uppercase xl:text-5xl'>{singleProduct.title}</h1>
+        <p className='text-lg'>{singleProduct.desc}</p>
+        <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options} />
+      </div>
+    </div>
   )
 }
 
-export default SingleProductPage    
+export default SingleProductPage
---
+++ src/components/Price.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import React from 'react'
+
+type PriceProps = {
+  price: number;
+  id: number;
+  options?: {
+    title: string;
+    additionalPrice: number;
+  }[];
+}
+
+const Price = ({ price, id, options }:PriceProps) => {
+
+    const [totalPrice, setTotalPrice] = React.useState(price);
+    const [quantity, setQuantity] = React.useState(1);
+    const [selectedOption, setSelectedOption] = React.useState(0);
+
+    React.useEffect(() => {
+        setTotalPrice(quantity * (price + (options && options[selectedOption] ? options[selectedOption].additionalPrice : 0)));
+    }, [quantity, selectedOption, options, price]);
+
+  return (
+    <div className='flex flex-col gap-4'>
+        {/* PRICE CONTAINER */}
+        <h2 className='text-2xl font-bold'>${totalPrice.toFixed(2)}</h2>
+
+        {/* OPTIONS CONTAINER */}
+        <div className='flex gap-4'>
+            {options && options.length > 0 && options.map((option, index) => (
+                <button
+                    key={index}
+                    className='min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md'
+                    style={{ backgroundColor: selectedOption === index ? 'rgb(248, 113,113)' : 'white',
+                        color: selectedOption === index ? 'white' : 'red' }}
+                    onClick={() => {
+                        setSelectedOption(index);
+                        setTotalPrice(price + option.additionalPrice);
+                    }}
+                    >
+                    {option.title}
+                </button>
+            ))}
+        </div>
+
+        {/* QUANTITY AND BUTTON CONTAINER */}
+        <div className='flex justify-between items-center'>
+            {/* QUANTITY CONTAINER */}
+            <div className='flex justify-between w-full p-3 ring-1 ring-red-500'>
+                <span>Quantity</span>
+                <div className='flex gap-3 items-center'>
+                    <button onClick={() => setQuantity(prev => Math.max(prev - 1, 1))}>{'<'}</button>
+                    <span>{quantity}</span>
+                    <button onClick={() => setQuantity(prev =>  Math.min(prev + 1, 9))}>{'>'}</button>
+                </div>
+            </div>
+            {/* ADD TO CART BUTTON */}
+            <button className='uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500'>Add to Cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default Price
