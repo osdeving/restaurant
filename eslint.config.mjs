fc897e78aff9cdb2352b341218edec259e02f008
Willams Sousa <osdeving@gmail.com>
2025-06-24 11:52:14 -0300

    feat: adicionar regras no eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,24 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
+// remover eslint no explicit any
+// ignore src/generated
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    ignores: ["**/node_modules/**", "**/dist/**", "**/build/**", "**/coverage/**", "**/generated/**", "**/src/generated/**"],
+  },
 ];
 
 export default eslintConfig;
